117213e0921e09eb16c069cf8b3379afc50c2f6c
Bashir <harrell.bashir@yahoo.com>
2019-03-05 11:59:20 +0800

    Added another project "Ride in Style Landing Page"

---
+++ script.js
@@ -0,0 +1,29 @@
+const chiButton = document.getElementById('chiButton');
+
+
+chiButton.onclick = (function (){
+    let noDisplay = document.getElementsByClassName('noDisplay');
+    let englishText = document.getElementsByClassName('english');
+    let chineseText = document.getElementsByClassName('chinese');
+
+    for (let i = 0; i <= englishText.length; i++){
+        chineseText[i].classList.toggle('noDisplay');
+        englishText[i].classList.toggle('noDisplay');
+        if( chiButton.innerHTML === "Chinese"){
+            chiButton.innerHTML = 'English';
+        }else {
+            chiButton.innerHTML = 'Chinese'
+        };
+    } 
+});
+
+// engButton.onclick = (function (){
+//     let noDisplay = document.getElementsByClassName('noDisplay');
+
+//     for (let i = 0; i <= chineseText.length; i++){
+//         console.log(chineseText[i].classList)
+//         noDisplay[i].classList.remove('noDisplay'); 
+//         chineseText[i].classList.add('noDisplay');
+//     }
+// });
+
